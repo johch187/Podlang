6803c251cf73747c44de653c76d79b08ba1953a9
Johan Chou Chen <johch187@student.liu.se>
2021-07-03 20:05:14 +0200

    cleaning out

--- server.py
+++ server.py
@@ -8,37 +8,36 @@ from flask import Flask, request, render_template, jsonify
 # Spotify API wrapper, documentation here: http://spotipy.readthedocs.io/en/latest/
 
 load_dotenv(find_dotenv())
-print(os.getenv('SPOTIPY_CLIENT_ID'))
-
 SPOTIPY_CLIENT_ID = os.environ.get("SPOTIPY_CLIENT_ID")
 SPOTIPY_CLIENT_SECRET = os.environ.get("SPOTIPY_CLIENT_SECRET")
 SESSION_SECRET = os.environ.get("SESSION_SECRET")
+SPOTIPY_REDIRECT_URI = os.environ.get("SPOTIPY_REDIRECT_URI")
 
 # Authenticate with Spotify using the Client Credentials flow
-sp = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
+client_credentials_manager = SpotifyClientCredentials()
+sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
-app = Flask(__name__, static_folder='public', template_folder='views')
+app = Flask(__name__, static_folder='public', template_folder='public')
 
 @app.route('/')
 def homepage():
-    return "hello world"
-
-@app.route('/authorize')
-def authorize():
-  client_id = app.config['CLIENT_ID']
-  redirect_uri = app.config['REDIRECT_URI']
-  scope = app.config['SCOPE']
-  state_key = createStateKey(15)
-  session['state_key'] = state_key
-
-  authorize_url = 'https://accounts.spotify.com/en/authorize?'
-  params = {'response_type': 'code', 'client_id': client_id,
-            'redirect_uri': redirect_uri, 'scope': scope, 
-            'state': state_key}
-  query_params = urlencode(params)
-  response = make_response(redirect(authorize_url + query_params))
-  return response
+    # Displays homepage
+    return render_template('index.html')
+  
+@app.route('/new_releases', methods=['GET'])
+def new_releases():
+  
+    # Use the country from the query parameters, if provided
+    if 'country' in request.args:
+        country = request.args['country']
+    else:
+        country = 'SE'
+    
+    # Send request to the Spotify API
+    new_releases = sp.new_releases(country=country, limit=20, offset=0)
+    
+    # Return the list of new releases
+    return jsonify(new_releases)
 
 if __name__ == '__main__':
     app.run()
-    
